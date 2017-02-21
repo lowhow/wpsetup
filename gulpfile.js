ebcd4dadaeb3686b21ca157961e07682f3d118dd
Low How <lowhow@gmail.com>
2017-02-22 04:11:50 +0800

    add db_name, db_password, wp_home prompt

--- gulpfile.js
+++ gulpfile.js
@@ -23,25 +23,67 @@ function setup_wp_config(content){
 		.replace( /collate_here/g, wp_config.DB_COLLATE );
 }
 
-gulp.task('wp:create', function(){
-	/**
-	 * populate wp-config.php
-	 */
-	gulp.src('wp-config.php')
-	.pipe( change( setup_wp_config ) )
-	.pipe( gulp.dest('./') )
 
-	/**
-	 * Run composer install
-	 */
-	composer();
+gulp.task('wp:config', function(){
+	if ( wp_config.DB_NAME == 'project_db' || wp_config.DB_NAME == 'mypassword' || wp_config.WP_HOME == 'http://dev.mysite.com' ){
+		gulp.src('')
+			.pipe( prompt.prompt([{
+				type: 'input',
+				name: 'DB_NAME',
+				message: 'Enter DB_NAME: ',
+				validate: function( DB_NAME ){
+		            if( DB_NAME == '' ){
+		                return false;
+		            }
+		            wp_config.DB_NAME = DB_NAME;
+		            return true;
+		        }
+			},{
+				type: 'input',
+				name: 'DB_PASSWORD',
+				message: 'Enter DB_PASSWORD: ',
+				validate: function( DB_PASSWORD ){
+		            if( DB_PASSWORD == '' ){
+		                return false;
+		            }
+		            wp_config.DB_PASSWORD = DB_PASSWORD;
+		            return true;
+		        }
+			},{
+				type: 'input',
+				name: 'WP_HOME',
+				message: 'Enter WP_HOME ("http://dev.mysite.com"):',
+				validate: function( WP_HOME ){
+		            if( WP_HOME == '' ){
+		                return false;
+		            }
+		            wp_config.WP_HOME = WP_HOME;
+		            return true;
+		        }
+			}], function(res){
+				/**
+				 * populate wp-config.php
+				 */
+				gulp.src('wp-config.php')
+				.pipe( change( setup_wp_config ) )
+				.pipe( gulp.dest('./') )
+			}))
+			.on('end', function(){
+				console.log( 'DB_NAME: ' + wp_config.DB_NAME + '\nDB_PASSWORD: ' + wp_config.DB_PASSWORD + '\nWP_HOME: ' + wp_config.WP_HOME );
+				/**
+				 * Create database
+				 */
+				var shellCommand = '/Applications/MAMP/Library/bin/mysql -u'+wp_config.DB_USER+' -p'+wp_config.DB_PASSWORD+' -e "CREATE DATABASE '+wp_config.DB_NAME+' DEFAULT CHARACTER SET '+wp_config.DB_CHARSET+' DEFAULt COLLATE '+wp_config.DB_COLLATE+'"';
+				gulp.src('')
+					.pipe(shell([shellCommand]))
+					.on('end', function(){
+						/**
+						 * Run composer install
+						 */
+						composer();
+					});
+				console.log('\n\x1b[32m%s\x1b[0m', 'Database created!');
 
-	/**
-	 * Create database
-	 */
-	var shellCommand = '/Applications/MAMP/Library/bin/mysql -u'+wp_config.DB_USER+' -p'+wp_config.DB_PASSWORD+' -e "CREATE DATABASE '+wp_config.DB_NAME+' DEFAULT CHARACTER SET '+wp_config.DB_CHARSET+' DEFAULt COLLATE '+wp_config.DB_COLLATE+'"';
-	gulp.src('').pipe(shell([shellCommand]));
-	console.log('\n\x1b[32m%s\x1b[0m', 'Database created!');
+			});
+	}
 });
-
-
